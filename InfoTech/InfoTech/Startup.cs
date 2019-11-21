45f26b164036f89981a39cb9043f8c2768b7fecf
Radu Tureac <radu.2reac@yahoo.com>
2019-11-21 18:29:59 +0200

    new scaffolded item Identity logIn/logOut/register/changeP + remove/comment emailSender in register

--- InfoTech/InfoTech/Startup.cs
+++ InfoTech/InfoTech/Startup.cs
@@ -56,6 +56,7 @@ namespace InfoTech
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
